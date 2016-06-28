69a4bd77a3a45a1cfe94a063cd50e5af54632418
Mike Meadows <mmeadow33@gmail.com>
2016-06-27 20:51:07 -0500

    employees starting to console.log out, cleaned up syntax with semicolons

--- javascripts/employees.js
+++ javascripts/employees.js
@@ -23,39 +23,58 @@
 function Employee (name1, name2) {
 	this.firstName = name1;
 	this.lastName = name2;
+	this.department = "";
+    this.job = "";
 }
 
-function Clinician () {
-
+function Job (job) {
+  this.jobname = job;
 }
 
-function Operations () {
+function Clinician (expert) {
+	this.expertise = expert;
+}
 
+function Operations (responsibility) {
+	this.responsibility = responsibility;
 }
 ///////need to make prototype///////
-Clinician.prototype = new Employee ();
-Operations.prototype = new Employee ();
+Clinician.prototype = new Job("Clinician");	
+Operations.prototype = new Job ("Operations");
 
 
 // Create a two-level prototype chain for creating departments in your business. 
 // Start off with a Department function. Then create two other functions named FieldOffice, 
 // and Headquarters that have Department on their prototype chain.
-function Department () {
-
+function Department (dept) {
+	this.deptName = [];
 }
 
-function FieldOffice () {
-
+function FieldOffice (city) {
+	this.city = city;
 }
 
 function Headquarters () {
-
+	this.Headquarters = "Nashville";
 }
 FieldOffice.prototype = new Department ();
 Headquarters.prototype = new Department ();
 
+///////////create new employee//////////////////////////
+var mikeM = new Employee("Mike", "Meadows");	/////////gives out first and last name;
+mikeM.Job = new Clinician("Brain Surgeon");
+mikeM.FieldOffice = new FieldOffice("Nashville");
+console.log("person:", mikeM);
 
+var DrewD = new Employee("Drew", "Davis");	/////////gives out first and last name;
+DrewD.Job = new Clinician("Brain Surgeon");
+DrewD.FieldOffice = new FieldOffice("Nashville");
+console.log("person:", DrewD);
 
+var GiannaC = new Employee("Gianna", "C");	/////////gives out first and last name;
+GiannaC.Job = new Clinician("Brain Surgeon");
+GiannaC.FieldOffice = new FieldOffice("Nashville");
+console.log("person:", GiannaC);
 
 
 
